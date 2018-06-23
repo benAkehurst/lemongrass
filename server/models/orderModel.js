a64570ec775278fb43b73ca204e4109676bd19e8
benAkehurst <benakehurst@hotmail.com>
2018-06-23 23:01:30 +0300

    Created Order Model

---
+++ server/models/orderModel.js
@@ -0,0 +1,31 @@
+'use strict';
+
+var debug = require('debug');
+var error = debug('reportModel:error');
+var log = debug('reportModel:log');
+
+var mongoose = require('mongoose');
+var mongooseUniqueValidator = require('mongoose-unique-validator');
+
+var Schema = mongoose.Schema,
+  ObjectId = Schema.ObjectId;
+
+var Order = new Schema({
+  items: {
+    type: Array
+  },
+  value: {
+    type: Number
+  }
+}, {
+  timestamps: true
+});
+
+
+Order.set('toJSON', {
+  transform: function (doc, ret, options) {
+    return ret;
+  }
+});
+
+module.exports = mongoose.model('Order', Order);
